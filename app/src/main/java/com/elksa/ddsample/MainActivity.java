ec3195b5f4dafe90375c187a39fadd0852633158
Alejandro Casanova <elksa2003@gmail.com>
2019-07-03 13:41:09 -0500

    Removed payments and added images module with basic heavy images for testing purposes

--- app/src/main/java/com/elksa/ddsample/MainActivity.java
+++ app/src/main/java/com/elksa/ddsample/MainActivity.java
@@ -1,8 +1,12 @@
 package com.elksa.ddsample;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnPictures = findViewById(R.id.btn_pictures);
+        btnPictures.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Show pictures module", Toast.LENGTH_SHORT).show();
+
+                SplitInstall
+            }
+        });
     }
 }
